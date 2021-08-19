59c39758d36a16944eca7962f75aeb0ac5cfd905
artem.zagoveliev <mexell96@gmail.com>
2021-08-19 10:57:25 +0300

    make it better

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use("/api", reviews);
 //   });
 // }
 
-const PORT = config.get("port") || 5000;
+// const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
