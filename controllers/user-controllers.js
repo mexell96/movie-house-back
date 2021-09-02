8adcc4ef94d8e341d1818481cdf3007c63be7d4b
artem.zagoveliev <mexell96@gmail.com>
2021-09-02 10:24:44 +0300

    added sameSite

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -37,6 +37,8 @@ class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
       return res.json({ message: "You entered", ...userData });
     } catch (e) {
@@ -62,6 +64,8 @@ class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
       return res.json(userData);
     } catch (e) {
